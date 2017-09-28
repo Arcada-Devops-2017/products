0eb2fc4303b7eb6a6d05a19e69bc193c79d5cbdc
Wilhelm Kinos <wkinos10@gmail.com>
2017-09-28 16:14:18 +0300

    changed test file4

--- test.php
+++ test.php
@@ -1,15 +1,12 @@
 <?php
+header('Content-Type: application/json; charset=utf-8');
 require_once("db.php");
-echo (1);
 $db = new DB("localhost", "products", "products", "products");
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-		echo (2);
         echo json_encode($db->query("SELECT * FROM products"));
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
         echo "POST";
-		echo (3);
 } else {
         http_response_code(405);
-		echo (4);
 }
 ?>
