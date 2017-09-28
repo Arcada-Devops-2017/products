4124e75133d65401bf401e89493159717ecfa759
Wilhelm Kinos <wkinos10@gmail.com>
2017-09-28 16:03:48 +0300

    changed test file2

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 require_once("db.php");
-$db = new DB("127.0.0.1", "products", "products", "products");
+$db = new DB("localhost", "products", "products", "products");
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
         echo json_encode($db->query("SELECT * FROM products"));
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
