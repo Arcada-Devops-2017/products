672e4c492b58c5f505b4ca65f540711b3f660553
Wilhelm Kinos <wkinos10@gmail.com>
2017-09-28 15:43:40 +0300

    added php test files

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+require_once("DB.php");
+$db = new DB("127.0.0.1", "products", "products", "products");
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        echo json_encode($db->query("SELECT * FROM products"));
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        echo "POST";
+} else {
+        http_response_code(405);
+}
+?>
