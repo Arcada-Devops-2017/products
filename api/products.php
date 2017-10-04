8a60144538c6359fc9003bdfd65f0c91d9adc8cd
Robin Walberg <robin.walberg@gmail.com>
2017-10-04 13:45:29 +0300

    added second api

---
+++ api/products.php
@@ -0,0 +1,12 @@
+<?php
+require_once(“../db.php");
+$id = $_GET[‘id’]; 
+$db = new DB("localhost", "products", "products", "products");
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        echo json_encode($db->query("SELECT * FROM products WHERE id =” . $id));
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        echo "POST";
+} else {
+        http_response_code(405);
+}
+?>
