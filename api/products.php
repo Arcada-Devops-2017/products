0e91da9d34847b34a07eb4ff51e3b53b8d13c071
Robin Walberg <robin.walberg@gmail.com>
2017-10-04 14:16:19 +0300

    fixed products.php

--- api/products.php
+++ api/products.php
@@ -4,8 +4,6 @@ $id = $_GET["id"];
 $db = new DB("localhost", "products", "products", "products");
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
         echo json_encode($db->query("SELECT * FROM products WHERE id =" . $id));
-} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        echo "POST";
 } else {
         http_response_code(405);
 }
