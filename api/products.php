430d7d5e146ed3316008af74fb7d6fffa7a6e411
Robin Walberg <robin.walberg@gmail.com>
2017-10-04 14:09:38 +0300

    fixed products.php

--- api/products.php
+++ api/products.php
@@ -1,9 +1,9 @@
 <?php
 require_once("db.php");
-$id = $_GET[‘id’]; 
+$id = $_GET["id"]; 
 $db = new DB("localhost", "products", "products", "products");
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-        echo json_encode($db->query("SELECT * FROM products WHERE id =” . $id));
+        echo json_encode($db->query("SELECT * FROM products WHERE id =" . $id));
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
         echo "POST";
 } else {
