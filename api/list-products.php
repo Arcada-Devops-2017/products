3d60881c489d4081ab30568d60aa55c8d3793792
Robin Walberg <robin.walberg@gmail.com>
2017-10-04 13:35:38 +0300

    added first api

---
+++ api/list-products.php
@@ -0,0 +1,11 @@
+<?php
+require_once("db.php");
+$db = new DB("localhost", "products", "products", "products");
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+        echo json_encode($db->query("SELECT * FROM products"));
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        echo "POST";
+} else {
+        http_response_code(405);
+}
+?>
