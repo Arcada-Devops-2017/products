75da568d7103e08453cc9342d54093c6103e6e70
Robin Walberg <robin.walberg@gmail.com>
2017-10-04 14:31:13 +0300

    fixed db4

--- api/list-products.php
+++ api/list-products.php
@@ -2,7 +2,7 @@
 require_once("db.php");
 $db = new DB("localhost", "products", "products", "products");
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-        echo json_encode($db->query("SELECT * FROM products”),JSON_UNESCAPED_SLASHES);
+        echo json_encode($db->query("SELECT * FROM products"),JSON_UNESCAPED_SLASHES);
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
         echo "POST";
 } else {
