e61d6dcacfa6969030f37218d7056d1d7ced4d72
Robin Walberg <robin.walberg@gmail.com>
2017-10-04 14:33:05 +0300

    fixed products

--- api/products.php
+++ api/products.php
@@ -3,7 +3,7 @@ require_once("db.php");
 $id = $_GET["id"]; 
 $db = new DB("localhost", "products", "products", "products");
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-        echo json_encode($db->query("SELECT * FROM products WHERE id =" . $id));
+        echo json_encode($db->query("SELECT * FROM products WHERE id =" . $id),JSON_UNESCAPED_SLASHES);
 } else {
         http_response_code(405);
 }
