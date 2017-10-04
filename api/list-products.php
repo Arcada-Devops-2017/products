2c67d871bc091faa54c9a45dd6469ef4330a98a2
Robin Walberg <robin.walberg@gmail.com>
2017-10-04 14:40:54 +0300

    fixed listproducts

--- api/list-products.php
+++ api/list-products.php
@@ -4,6 +4,7 @@ require_once("db.php");
 $db = new DB("localhost", "products", "products", "products");
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
 	if ($sort != null){
+echo “1”;
 		echo json_encode($db->query("SELECT * FROM products ORDER BY " .$sort),JSON_UNESCAPED_SLASHES);
 	}
 	else {
